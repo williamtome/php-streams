f7720ceed8e3429ffb850655cf7f48f01a463d83
William <william.tomenh@gmail.com>
2021-09-23 19:24:05 -0300

    feat: import file handle.

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
+use WilliamT\PhpStreams\FileHandle;
+
 require_once 'FileHandle.php';
 
 try {
-    $courses = new \WilliamT\PhpStreams\FileHandle('courses.txt', 'a');
+    $courses = new FileHandle('courses.txt', 'a');
 
     $courses->write("\nabc123");
 } catch (\Exception $e) {
