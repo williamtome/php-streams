9ff8e6641dedc282f7a8490c75802498bafaecf3
William <william.tomenh@gmail.com>
2021-10-04 20:31:18 -0300

    feat: change HTTP request.

---
+++ httpbin.php
@@ -0,0 +1,14 @@
+<?php
+
+$context = stream_context_create([
+    'http' => [
+        'method' => 'POST',
+        'header' => "X-From: PHP\r\nContent-Type: text/plain",
+        'content' => 'Teste de corpo',
+    ]
+]);
+echo file_get_contents(
+    "http://httpbin.org/post",
+    false,
+    $context
+);
